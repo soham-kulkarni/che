968a4b2cdb7bbc6c62afd44872ee58611dafffd3
Oleksii Kurinnyi <okurinnyi@codenvy.com>
2017-05-22 10:55:29 +0300

    CHE-3048: add namespace-selector component
    Signed-off-by: Oleksii Kurinnyi <okurinnyi@codenvy.com>

--- dashboard/src/app/workspaces/create-workspace/namespace-selector/namespace-selector.directive.ts
+++ dashboard/src/app/workspaces/create-workspace/namespace-selector/namespace-selector.directive.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2017 Codenvy, S.A.
+ * Copyright (c) 2015-2016 Codenvy, S.A.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,17 +11,20 @@
 'use strict';
 
 /**
- * Defines the class for filter selector widget.
- * @author Ann Shumilova
+ * Defines a directive for displaying stack selector widget.
+ *
+ * @author Oleksii Kurinnyi
  */
-export class CheFilterSelector implements ng.IDirective {
 
+export class NamespaceSelector implements ng.IDirective {
   restrict: string = 'E';
+  templateUrl: string = 'app/workspaces/create-workspace/namespace-selector/namespace-selector.html';
+  replace: boolean = true;
+
+  controller: string = 'NamespaceSelectorController';
+  controllerAs: string = 'namespaceSelectorController';
+
   bindToController: boolean = true;
-  templateUrl: string = 'components/widget/filter-selector/che-filter-selector.html';
-  controller: string = 'CheFilterSelectorController';
-  controllerAs: string = 'cheFilterSelectorController';
-  require: Array<string> = ['ngModel'];
 
   scope: {
     [propName: string]: string
@@ -33,11 +36,7 @@ export class CheFilterSelector implements ng.IDirective {
    */
   constructor() {
     this.scope = {
-      valueModel: '=ngModel',
-      values: '=cheValues',
-      isDisabled: '=cheDisabled',
-      onChange: '&cheOnChange',
-      width: '@?cheWidth'
+      onNamespaceChange: '&'
     };
   }
 }
