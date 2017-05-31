f6b65443256df4d9c8a45fac4312a94e21d8567d
Igor Vinokur <ivinokur@codenvy.com>
2017-05-31 18:12:43 +0300

    commit

--- agents/git-ssh-keys-injector/src/main/resources/org.eclipse.che.git.ssh.script.sh
+++ agents/git-ssh-keys-injector/src/main/resources/org.eclipse.che.git.ssh.script.sh
@@ -11,19 +11,29 @@
 
 script="/home/user/.ssh/git.sh"
 
-token_suffix=$(if [ "$USER_TOKEN" != "dummy_token" ]; then echo "\"&token=$USER_TOKEN\""; fi)
+token=$(if [ "$USER_TOKEN" != "dummy_token" ]; then echo "$USER_TOKEN"; fi)
 che_host=$(cat /etc/hosts | grep che-host | awk '{print $1;}')
 api_url=$(if [ "$CHE_API" != "http://che-host:8080/wsmaster/api" ]; then echo "$CHE_API"; else echo "$che_host:8080/api"; fi)
 
-echo '#!/bin/bash' > $script
-echo 'host=$(echo $(if [ "$1" = "-p" ]; then echo "$3" ; else echo "$1"; fi) | sed -e "s/git@//")' >> $script
-echo 'token_suffix='$token_suffix >> $script
+CURL_INSTALLED=false
+WGET_INSTALLED=false
+command -v curl >/dev/null 2>&1 && CURL_INSTALLED=true
+command -v wget >/dev/null 2>&1 && WGET_INSTALLED=true
+
+# no curl, no wget, install curl
+if [ ${CURL_INSTALLED} = false ] && [ ${WGET_INSTALLED} = false ]; then
+  PACKAGES=${PACKAGES}" curl";
+  CURL_INSTALLED=true
+fi
+
+echo 'host=$(echo $(if [ "$1" = "-p" ]; then echo "$3" ; else echo "$1"; fi) | sed -e "s/git@//")' > $script
+echo 'token='$token >> $script
 echo 'api_url='$api_url >> $script
-echo 'ssh_key=$(curl -s "$api_url/ssh/vcs/find?name=$host$token_suffix"| grep -Po '\''"privateKey":.*?[^\\\\]",'\''| sed -e "s/\"privateKey\":\"//")' >> $script
+echo 'ssh_key=$(curl -s "$api_url/ssh/vcs/find?name=$host$(if [ -n "$token" ]; then echo "&token=$token"; fi)"| grep -Po '\''"privateKey":.*?[^\\\\]",'\''| sed -e "s/\"privateKey\":\"//" | sed -e "s/\\\\\u003d/=/")' >> $script
 echo 'if [ -n "$ssh_key" ]' >> $script
 echo 'then' >> $script
 echo '    key_file=$(mktemp)' >> $script
-echo '    echo -en "$ssh_key" > "$key_file"' >> $script
+echo '    echo "$ssh_key" > "$key_file"' >> $script
 echo '    ssh -i "$key_file" "$@"' >> $script
 echo '    rm "$key_file"' >> $script
 echo 'else' >> $script
@@ -32,12 +42,12 @@ echo 'fi' >> $script
 
 chmod +x $script
 
-user_name=$(curl -s "$api_url/preferences$token_suffix" | grep -Po '"git.committer.name":.*?[^\\]",' | sed -e "s/\"git.committer.name\":\"//" | sed -e "s/\",//")
-user_email=$(curl -s "$api_url/preferences$token_suffix" | grep -Po '"git.committer.email":.*?[^\\]",' | sed -e "s/\"git.committer.email\":\"//" | sed -e "s/\",//")
-git config --global user.name "$user_name"
-git config --global user.email "$user_email"
+user_name="$(curl -s "$api_url/preferences$(if [ -n "$token" ]; then echo "?token=$token"; fi)" | grep -Po '"git.committer.name":.*?[^\\]",' | sed -e "s/\"git.committer.name\":\"//" | sed -e "s/\",//")"
+user_email="$(curl -s "$api_url/preferences$(if [ -n "$token" ]; then echo "?token=$token"; fi)" | grep -Po '"git.committer.email":.*?[^\\]",' | sed -e "s/\"git.committer.email\":\"//" | sed -e "s/\",//")"
+git config --global user.name \""$user_name"\"
+git config --global user.email \""$user_email"\"
 
-if [ -z $(cat /home/user/.bashrc | grep GIT_SSH) ]
+if [ -z "$(cat /home/user/.bashrc | grep GIT_SSH)" ]
 then
     printf '\n export GIT_SSH='$script >> /home/user/.bashrc
 fi
